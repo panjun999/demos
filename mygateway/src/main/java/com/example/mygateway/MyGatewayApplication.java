7209a573e40b268572f8d8bfbd3c456e3278ff44
panjun <fjswpj@163.com>
2020-03-18 01:13:54 +0800

    第一次修改

--- mygateway/src/main/java/com/example/mygateway/MyGatewayApplication.java
+++ mygateway/src/main/java/com/example/mygateway/MyGatewayApplication.java
@@ -13,6 +13,7 @@ import reactor.core.publisher.Mono;
 public class MyGatewayApplication {
 
     public static void main(String[] args) {
+		String s="123";
         SpringApplication.run(MyGatewayApplication.class, args);
     }
 
